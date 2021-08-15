6e53442201381451f5aee2f928c664ca9e5c27b6
shrilakshmishastry <shrilakshmishastry@gmail.com>
2021-08-15 18:11:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+        <h1 className="text-5xl text-green-400">hello world</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
